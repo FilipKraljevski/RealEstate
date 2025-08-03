6bcc3a34dfdda5fb06e1efd6411cc7003ae8ca2d
Filip Kraljevski <filipce001@gmail.com>
2025-08-03 10:43:54 +0200

    Real Estate Page

--- Frontend/src/apps/realEstateView/RealEstate.tsx
+++ Frontend/src/apps/realEstateView/RealEstate.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Container, Divider, FormControl, Grid, InputAdornment, InputLabel, MenuItem, Select, TextField, Typography, useMediaQuery, useTheme } from '@mui/material'
-import { createLazyRoute } from '@tanstack/react-router'
+import { Box, Button, Card, CardActionArea, CardContent, CardHeader, CardMedia, Container, Divider, FormControl, Grid, InputAdornment, InputLabel, MenuItem, Select, TablePagination, TextField, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { createLazyRoute, Link } from '@tanstack/react-router'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { EstateType } from '../../common/Domain/EstateType'
 import { PurchaseType } from '../../common/Domain/PurchaseType'
 import { Country } from '../../common/Domain/Country'
-import { enumToOptions } from '../../common/Logic/EnumHelper'
+import { enumToOptions, getEnumTypeKey } from '../../common/Logic/EnumHelper'
 
 export const Route = createLazyRoute('/RealEstate')({
     component: RealEstate,
@@ -32,10 +32,12 @@ export default function RealEstate() {
 
     const { t } = useTranslation()
     const theme = useTheme()
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const [openFilters, setOpenFilters] = useState(true)
     const [filter, setFilters] = useState<Partial<Filter>>({})
     const [cityOptions, setCityOptions] = useState<Item[]>([])
+    const [page, setPage] = useState(0)
+    const [rowsPerPage, setRowsPerPage] = useState(10)
 
     const purchaseOptions = enumToOptions(PurchaseType)
     const estateOptions = enumToOptions(EstateType)
@@ -66,6 +68,20 @@ export default function RealEstate() {
           }))
     }
 
+    const handleOnChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+        setPage(newPage);
+    };
+    
+    const handleOnChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleOnSubmit = (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
     const Adornment = (value: string) => {
         return <InputAdornment position="end">{value}</InputAdornment>
     }
@@ -75,19 +91,20 @@ export default function RealEstate() {
             <Typography variant='h4' sx={{mb: '1%'}}>Estates</Typography>
             <Divider />
 
-            <Box sx={{mt: '1%'}}>
+            <Box sx={{mt: 1}}>
                 {isSmallScreen && <Button type='button' variant='contained' sx={{mt: '1%'}} onClick={() => setOpenFilters(!openFilters)}>
-                    Open filters
+                    {t('RealEstate.OpenFilter')}
                 </Button>}
-                {openFilters &&  <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }} sx={{justifyContent: 'space-between'}}>
-                    <Grid size={3}>
+                {openFilters &&  <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }} sx={{justifyContent: 'space-between', mt: 1, flexGrow: 1}}
+                    component="form" noValidate autoComplete="off" onSubmit={handleOnSubmit} width='100%' >
+                    <Grid size={3} >
                         <FormControl fullWidth>
-                        <InputLabel id='filter.estateFor'>{t(`form.${filter.estateFor}`)}</InputLabel>
-                        <Select value={filter.estateFor} labelId='filter.estateFor' label={t(`form.${filter.estateFor}`)}
+                        <InputLabel id='filter.estateFor'>{t(`filter.estateFor`)}</InputLabel>
+                        <Select value={filter.estateFor} labelId='filter.estateFor' label={t(`filter.${filter.estateFor}`)}
                             onChange={(value) => handleOnChangeFilters("estateFor", value)}>
                             {purchaseOptions.map((item: Item) => {
                                 return (
-                                    <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                    <MenuItem key={item.value} value={item.value}>{t(`option.${item.label}`)}</MenuItem>
                                 )
                             })}
                         </Select>
@@ -95,12 +112,12 @@ export default function RealEstate() {
                     </Grid>
                     <Grid size={3}>
                         <FormControl fullWidth>
-                        <InputLabel id='filter.estateType'>{t(`form.${filter.estateType}`)}</InputLabel>
-                        <Select fullWidth value={filter.estateType} labelId='filter.estateType' label={t(`form.${filter.estateType}`)}
+                        <InputLabel id='filter.estateType'>{t(`filter.estateType`)}</InputLabel>
+                        <Select fullWidth value={filter.estateType} labelId='filter.estateType' label={t(`filter.${filter.estateType}`)}
                             onChange={(value) => handleOnChangeFilters("estateType", value)}>
                             {estateOptions.map((item: Item) => {
                                 return (
-                                    <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                    <MenuItem key={item.value} value={item.value}>{t(`option.${item.label}`)}</MenuItem>
                                 )
                             })}
                         </Select>
@@ -108,12 +125,12 @@ export default function RealEstate() {
                     </Grid>
                     <Grid size={3}>
                         <FormControl fullWidth>
-                        <InputLabel id='filter.country'>{t(`form.${filter.country}`)}</InputLabel>
-                        <Select fullWidth value={filter.country} labelId='filter.country' label={t(`form.${filter.country}`)}
+                        <InputLabel id='filter.country'>{t(`filter.country`)}</InputLabel>
+                        <Select fullWidth value={filter.country} labelId='filter.country' label={t(`filter.${filter.country}`)}
                             onChange={(value) => handleOnChangeFilters("country", value)}>
                             {countryOptions.map((item: Item) => {
                                 return (
-                                    <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                    <MenuItem key={item.value} value={item.value}>{t(`option.${item.label}`)}</MenuItem>
                                 )
                             })}
                         </Select>
@@ -121,8 +138,8 @@ export default function RealEstate() {
                     </Grid>
                     <Grid size={3}>
                         <FormControl fullWidth>
-                        <InputLabel id='filter.city'>{t(`form.${filter.city}`)}</InputLabel>
-                        <Select fullWidth value={filter.city} labelId='filter.city' label={t(`form.${filter.city}`)}
+                        <InputLabel id='filter.city'>{t(`filter.city`)}</InputLabel>
+                        <Select fullWidth value={filter.city} labelId='filter.city' label={t(`filter.${filter.city}`)}
                             onChange={(value) => handleOnChangeFilters("city", value)}>
                             {cityOptions.map((item: Item) => {
                                 return (
@@ -134,8 +151,8 @@ export default function RealEstate() {
                     </Grid>
                     <Grid size={3}>
                         <FormControl fullWidth>
-                        <InputLabel id='filter.agency'>{t(`form.${filter.agency}`)}</InputLabel>
-                        <Select fullWidth value={filter.agency} labelId='filter.agency' label={t(`form.${filter.agency}`)}
+                        <InputLabel id='filter.agency'>{t(`filter.agency`)}</InputLabel>
+                        <Select fullWidth value={filter.agency} labelId='filter.agency' label={t(`filter.${filter.agency}`)}
                             onChange={(value) => handleOnChangeFilters("agency", value)}>
                             {agencyOptions.map((item: Item) => {
                                 return (
@@ -146,23 +163,66 @@ export default function RealEstate() {
                         </FormControl>
                     </Grid>
                     <Grid size={2}> 
-                        <TextField label={filter.fromArea} value={filter.fromArea} fullWidth type='number'
+                        <TextField label={t(`filter.fromArea`)} value={filter.fromArea} fullWidth type='number'
                             onChange={(e) => handleOnChangeFilters("fromArea", e.target.value)}></TextField>
                     </Grid>
                     <Grid size={2}> 
-                        <TextField label={filter.toArea} value={filter.toArea} fullWidth type='number'
+                        <TextField label={t(`filter.toArea`)} value={filter.toArea} fullWidth type='number'
                             onChange={(e) => handleOnChangeFilters("toArea", e.target.value)}></TextField>
                     </Grid>
                     <Grid size={2}> 
-                        <TextField label={filter.fromPrice} value={filter.fromPrice} fullWidth type='number'
+                        <TextField label={t(`filter.fromPrice`)} value={filter.fromPrice} fullWidth type='number'
                             onChange={(e) => handleOnChangeFilters("fromPrice", e.target.value)} slotProps={{ input: {endAdornment: Adornment("€")}}}></TextField>
                     </Grid>
                     <Grid size={2}> 
-                        <TextField label={filter.toPrice} value={filter.toPrice} fullWidth type='number'
+                        <TextField label={t(`filter.toPrice`)} value={filter.toPrice} fullWidth type='number'
                             onChange={(e) => handleOnChangeFilters("toPrice", e.target.value)} slotProps={{ input: {endAdornment: Adornment("€")}}}></TextField>
                     </Grid>
+                    <Button type='submit' variant='contained'>{t('RealEstate.Submit')}</Button>
                 </Grid>}
-                <Divider />
+
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%', mt: 1 }}>
+                    {itemData.map(item => (
+                        <Card key={item.id} sx={{ width: '100%', display: 'flex', flexDirection: 'column'}}>
+                            <CardHeader title={item.name} sx={{ backgroundColor: 'lightgray'}}/>
+                            <CardActionArea component={Link} to={`/AgencyDetails/${item.id}`} sx={{ display: 'flex', 
+                                flexDirection:{ xs: 'column', sm: 'row' }, alignItems: 'center', textDecoration: 'none' }} >
+                                <CardMedia component="img" image={item.img[0]} alt={item.name} sx={{width: 350, objectFit: 'cover'}} />
+                                <CardContent sx={{ flexGrow: 1 }}>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.EstateFor`)}</strong> {t(`Purchase.${getEnumTypeKey(item.estateFor, PurchaseType)}`)}
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.EstateType`)}</strong> {t(`Estate.${getEnumTypeKey(item.estateType, EstateType)}`)}
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.Agency`)}</strong> {item.agency.name}
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.Country`)}</strong> {t(`Country.${getEnumTypeKey(item.country, Country)}`)}
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.Location`)}</strong> {item.location}
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.Area`)}</strong> {item.area} m²
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        <strong>{t(`RealEstate.Price`)}</strong> {item.price} €
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {item.description.substring(0, 100)}…
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    ))}
+                </Box>
+            </Box>
+
+            <Box sx={{display: 'flex', justifyContent: 'center', mt: 1}}>
+                <TablePagination labelRowsPerPage={t('RealEstate.RowsPerPage')} component="div" count={100} page={page} 
+                    onPageChange={handleOnChangePage} rowsPerPage={rowsPerPage} onRowsPerPageChange={handleOnChangeRowsPerPage}/>
             </Box>
         </Container>
     )
@@ -180,5 +240,56 @@ const itemDataCities: Item[] = [
     {
         value: "id",
         label: "Skopje"
+    },
+]
+
+const itemData = [
+    {
+        id: "Id",
+        img: ["/GramadaLogoUrl.png"],
+        name: "luxiourus",
+        estateFor: 1,
+        estateType: 1,
+        agency: {
+            id: "Id",
+            name: "Gramada Agency"
+        },
+        country: 1,
+        location: "Aerodrom, Skopje",
+        area: 100,
+        price: 100000 ,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    },
+    {
+        id: "Id",
+        img: ["/GramadaLogoUrl.png"],
+        name: "luxiourus",
+        estateFor: 1,
+        estateType: 1,
+        agency: {
+            id: "Id",
+            name: "Gramada Agency"
+        },
+        country: 1,
+        location: "Aerodrom, Skopje",
+        area: 100,
+        price: 100000 ,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+    },
+    {
+        id: "Id",
+        img: ["/GramadaLogoUrl.png"],
+        name: "luxiourus",
+        estateFor: 1,
+        estateType: 1,
+        agency: {
+            id: "Id",
+            name: "Gramada Agency"
+        },
+        country: 1,
+        location: "Aerodrom, Skopje",
+        area: 100,
+        price: 100000 ,
+        description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
     },
 ]
